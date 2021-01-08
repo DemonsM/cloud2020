c31a6d6c015a9d0c2e23e9975c825f20825f66a8
梅涛 <573945966@qq.com>
2021-01-08 16:03:16 +0800

    注册服务

--- cloud-consumer-order-80/src/main/java/com/ink/springcloud/OrderMain80.java
+++ cloud-consumer-order-80/src/main/java/com/ink/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.ink.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author MT
  * @date 2021/1/8 10:48
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
