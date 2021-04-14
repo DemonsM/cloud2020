bed0c5f21a2025ad01e229416faa8cec79df46fe
梅涛 <573945966@qq.com>
2021-04-14 16:07:58 +0800

    gateway

---
+++ cloud-gateway/src/main/java/com/ink/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author MT
+ * @date 2021/1/26 10:27
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
